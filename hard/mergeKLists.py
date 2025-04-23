4c80fd0adcd0ba71f8c566d621b30de92db27b58
oriItzh <160385751+oriItzh@users.noreply.github.com>
2025-04-24 01:54:25 +0300

    Added solution to merge k lists

---
+++ hard/mergeKLists.py
@@ -0,0 +1,65 @@
+
+from utils.ListNode import ListNode
+import heapq
+
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]: # type: ignore
+        """
+        Problem: Merge k sorted Lists; leetcode #23
+        You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+        Merge all the linked-lists into one sorted linked-list and return it.
+
+        
+
+        Example 1:
+
+        Input: lists = [[1,4,5],[1,3,4],[2,6]]
+        Output: [1,1,2,3,4,4,5,6]
+        Explanation: The linked-lists are:
+        [
+        1->4->5,
+        1->3->4,
+        2->6
+        ]
+        merging them into one sorted list:
+        1->1->2->3->4->4->5->6
+        Example 2:
+
+        Input: lists = []
+        Output: []
+        Example 3:
+
+        Input: lists = [[]]
+        Output: []
+        
+
+        Constraints:
+
+        k == lists.length
+        0 <= k <= 104
+        0 <= lists[i].length <= 500
+        -104 <= lists[i][j] <= 104
+        lists[i] is sorted in ascending order.
+        The sum of lists[i].length will not exceed 104.
+        """
+        
+        if not lists:
+            return None
+        heap = []
+        for i, node in enumerate(lists):
+            if node:
+                heapq.heappush(heap, (node.val, i, node))
+
+        dummy = ListNode(0)
+        res_head = dummy
+
+        while heap:
+            val, i, node = heapq.heappop(heap)
+            res_head.next = node
+            res_head = res_head.next
+            if node.next:
+                heapq.heappush(heap, (node.next.val, i, node.next))
+
+        return dummy.next
--- medium/addTwoNumbers.py
+++ medium/addTwoNumbers.py
@@ -1,14 +1,11 @@
-from typing import Optional
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+from utils.ListNode import ListNode
 
 
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        """You are given two non-empty linked lists representing two non-negative integers.
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]: # type: ignore
+        """
+        Problem: Add Two Numbers ; leetcode #2
+        You are given two non-empty linked lists representing two non-negative integers.
         The digits are stored in reverse order, and each of their nodes contains a single digit.
         Add the two numbers and return the sum as a linked list.
 
