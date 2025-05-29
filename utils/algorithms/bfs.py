b84ed0f7fe6f90f69a9cffaf227d12b02739a4f2
Ori Itzhaki <oriitzhaki999@gmail.com>
2025-05-29 13:10:02 +0300

    added bfs

---
+++ utils/algorithms/bfs.py
@@ -0,0 +1,19 @@
+from collections import defaultdict, deque
+from typing import List
+class GraphTraversal:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def bfs(self, start):
+        visited = set()
+        queue = deque([start])
+        traversal_order = []
+
+        while queue:
+            vertex = queue.popleft()
+            if vertex not in visited:
+                visited.add(vertex)
+                traversal_order.append(vertex)
+                queue.extend(neighbor for neighbor in self.graph[vertex] if neighbor not in visited)
+
+        return traversal_order
