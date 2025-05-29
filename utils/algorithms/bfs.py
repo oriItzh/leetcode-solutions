d82006b40852cf1b67b2ebd931424eff9bc573b1
Ori Itzhaki <oriitzhaki999@gmail.com>
2025-05-29 13:20:47 +0300

    added dfs

--- utils/algorithms/bfs.py
+++ utils/algorithms/bfs.py
@@ -1,12 +1,20 @@
-from collections import defaultdict, deque
-from typing import List
-
+from collections import deque
+from typing import List, Any, Dict
 
 class GraphTraversal:
-    def __init__(self, graph):
+    def __init__(self, graph: Dict[Any, List[Any]]):
+        """
+        Initialize with a graph adjacency list.
+        :param graph: Dictionary mapping node to list of neighbors.
+        """
         self.graph = graph
 
-    def bfs(self, start):
+    def bfs(self, start: Any) -> List[Any]:
+        """
+        Perform Breadth-First Search (BFS) starting from the given node.
+        :param start: The starting node for BFS.
+        :return: List of nodes in the order they are visited.
+        """
         visited = set()
         queue = deque([start])
         traversal_order = []
@@ -22,4 +30,23 @@ class GraphTraversal:
                     if neighbor not in visited
                 )
 
+        return traversal_order
+
+    def dfs(self, start: Any) -> List[Any]:
+        """
+        Perform Depth-First Search (DFS) starting from the given node.
+        :param start: The starting node for DFS.
+        :return: List of nodes in the order they are visited.
+        """
+        visited = set()
+        traversal_order = []
+
+        def _dfs(node):
+            if node not in visited:
+                visited.add(node)
+                traversal_order.append(node)
+                for neighbor in self.graph[node]:
+                    _dfs(neighbor)
+
+        _dfs(start)
         return traversal_order
